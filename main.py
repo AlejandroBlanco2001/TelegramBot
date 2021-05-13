bf89001e19fb5c0417d72c7835f1034b8ff1c01e
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2021-05-13 15:28:44 -0500

    Implementing primary functions

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import requests
 import logging  
+import config
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-bot = Bot(token=TOKEN)
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 logger = loggin.getLogger(__name__)
 
+def start(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="SIUUU")
+
 def main():
-    updater = Updater("TOKEN", user_context=true)
+    
+    updater = Updater(config.TOKEN, user_context=true)
     dp = updater.dispatcher
     
+    dp.add_handler(CommandHandler('start', start))
+    updater.start_polling()
+
 if __name__ == '__main__':
     main()
