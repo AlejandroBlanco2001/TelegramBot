b8624504876e2884544f3c579cc7756560280fac
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2021-05-18 08:07:18 -0500

    Latex display preview implemented

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import config
+import sys
 import sympy as sp
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -10,10 +11,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 
 logger = logging.getLogger(__name__)
 
-
 bot = Bot(token=config.TOKEN)
 
-def start(update, context):
+def start(update: Update, context: CallbackContext) -> None:
     username = update.message.chat["first_name"]
     bot.send_message(chat_id=update.effective_chat.id, text=f"Aló, con Dorian, {username} \n" +
                                                                     "cuento con los siguentes comandos \n" +
@@ -24,10 +24,8 @@ def start(update, context):
                                                                     "\t /subsecuencia \n" +
                                                                     "\t /dameOpciones")
 
-
-def echo(update, context):
+def echo(update: Update, context: CallbackContext) -> None:
     start(update, context)
-
 
 def send_options(update: Update, context: CallbackContext) -> None:
     keyboard = [
@@ -43,10 +41,8 @@ def send_options(update: Update, context: CallbackContext) -> None:
     reply = InlineKeyboardMarkup(keyboard)
     update.message.reply_text("Elige una función", reply_markup=reply)
 
-
 def send_help(update: Update, context: CallbackContext) -> None:
     return None
-
 
 def menu(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
@@ -70,30 +66,19 @@ def menu(update: Update, context: CallbackContext) -> None:
         bot.send_message(chat_id=update.effective_chat.id, text="El comando de Subsecuencia se encarga de recibir" +
                                                                         "tal tal tal tal")
 
-
 def send_help(update: Update, context: CallbackContext) -> None:
     pass
 
-
 def recurrencia(update: Update, context: CallbackContext) -> None:
-    equation = " ".join(context.args).strip().split(" ")[0]
+    equation = " ".join(context.args).strip()
     function = resolve_characteristical_polynomial(equation)
-    latex = sp.latex(function)
-    print(latex)
-    plt.text(0, 0.6, r"$%s$" % latex, fontsize = 40)                                  
-
-    #hide axes                                                                      
-    fig = plt.gca()                                                                 
-    fig.axes.get_xaxis().set_visible(False)                                         
-    fig.axes.get_yaxis().set_visible(False)                                         
-    plt.savefig("function.png")
+    sp.preview(function,viewer="file",filename="function.png", euler=False,
+            dvioptions=["-T", "tight", "-z", "0", "--truecolor", "-D 600", "-bg", "Transparent"])
     bot.send_photo(chat_id=update.effective_chat.id, photo=open(
             "function.png", 'rb'), caption=f"Función que representa el polinomio caracteristico {equation}" + "\n" + str(function))
-
-
+    
 def grafo(update: Update, context: CallbackContext) -> None:
-    vertices, aristas, maximum_degree = " ".join(
-        context.args).strip().split(" ")
+    vertices, aristas, maximum_degree = context.args
     try:
         vertices = int(vertices)
         aristas = int(aristas)
@@ -101,11 +86,14 @@ def grafo(update: Update, context: CallbackContext) -> None:
     except Exception:
         bot.send_message(chat_id=update.effective_chat.id,
                                 text="Alguno de los valores ingresados no es un numero")
-    if vertices < 0 or aristas < 0 or maximum_degree < 0:
+    if vertices <= 0 or aristas <= 0 or maximum_degree <= 0:
         bot.send_message(chat_id=update.effective_chat.id, text="Algun valor ingresado es negativo,"
                                 + "por favor ingresa numeros positivos")
+    bot.send_message(chat_id=update.effective_chat.id,
+                                text="Cogela con su avena y su pitillo, estoy procesando el grafo")
     Graph = generate_graph(vertices, aristas, maximum_degree)
     if Graph != None:
+        plt.clf() # In case that the figure has something in it 
         nx.draw(Graph)
         plt.savefig("graph.png")
         plt.close()
@@ -115,7 +103,12 @@ def grafo(update: Update, context: CallbackContext) -> None:
         bot.send_message(chat_id=update.effective_chat.id,
                                 text="No es posible hacer un grafo con los parametros dados.")
 
-
+def fibonazzi(update: Update, context: CallbackContext) -> None:
+    if checkOrder(context.args):
+        subsequence = sub_fibonacci_sequence(context.args)
+    else:
+        bot.send_message(chat_id=update.effective_chat.id, text="La secuencia debe estar en orden ascedente")
+        
 def main():
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -123,7 +116,7 @@ def main():
     updater = Updater(config.TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    #
+    # Helpe functions
     dp.add_handler(CommandHandler('Timbrar', start))
     dp.add_handler(CommandHandler('LlamaFiscalia', send_help))
     dp.add_handler(CommandHandler("dameOpciones", send_options))
@@ -131,6 +124,8 @@ def main():
     # Math functions
     dp.add_handler(CommandHandler('grafo', grafo))
     dp.add_handler(CommandHandler('recurrencia', recurrencia))
+    dp.add_handler(CommandHandler("fibonacci", fibonazzi))
+
 
     # Listener of the toolbox command
     dp.add_handler(CallbackQueryHandler(menu))
@@ -141,7 +136,6 @@ def main():
     dp.add_handler(CommandHandler('test', help))
     updater.start_polling()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ import sympy as sp
 from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor
 transformations = (standard_transformations + (implicit_multiplication_application,) + (convert_xor,))
 
-
 """
 Function that return a simple graph
 
@@ -17,16 +16,16 @@ maximum_degree: Maximum degree number for the vertices
 def generate_graph(vertices : int, edges: int, maximum_grade: int) -> nx.Graph:
     if edges > maximum_grade*vertices/2:
         return None
-    elif edges > vertices*(vertices-1)/2:
+    elif edges > vertices*(vertices-1)/2: # Test more edges than a complete graph
         return None
-    elif edges == vertices*(vertices-1)/2:
+    elif edges == vertices*(vertices-1)/2: # Complete graph
         return nx.complete_graph(vertices)
     else:
         sequence = generate_degree_sequence(vertices,edges,maximum_grade)
         try:
             G = nx.random_degree_sequence_graph(sequence)
         except Exception:
-            pass
+            return None
     return G
 
 """
@@ -44,20 +43,20 @@ maximum_degree: Maximum degree number for the vertices
 """
 def generate_degree_sequence(vertices : int, edges: int, maximum_grade: int) -> list:
     accumulate_degree = 0
-    degree_sequence = np.random.randint(1,maximum_grade, size=(vertices))
-    while sum(degree_sequence) != edges*2: # Handshake lema
-        degree_sequence = np.random.randint(1,maximum_grade, size=(vertices))        
+    degree_sequence = np.random.randint(maximum_grade+1, size=(vertices)) 
+    while sum(degree_sequence) != edges*2: # Handshake lemma
+        degree_sequence = np.random.randint(maximum_grade+1, size=(vertices))        
     return degree_sequence
 
 """
 Function that generates the solution of recurrency relation
-given his characteristical polynomial
+given his characteristical polynomial in terms of constants
 
 equation : String that represents the characteristical polynomial
 """
 def resolve_characteristical_polynomial(equation : str) -> sp.Function : 
     x = sp.symbols('x')
-    f = parse_expr(equation, transformations=transformations)
+    f = parse_expr(equation, transformations=transformations) 
     roots = sp.solve(f,x)
     mults = {root:roots.count(root) for root in roots}
     function_ans = list()
@@ -72,3 +71,43 @@ def resolve_characteristical_polynomial(equation : str) -> sp.Function :
     ans = "+".join(function_ans)
     y = parse_expr(ans,transformations=transformations)
     return y    
+
+"""
+Function that generates the solution of recurrency relation
+given his characteristical polynomial
+
+equation : String that represents the characteristical polynomial
+initial_values: base cases of the recurrence relation
+"""
+# Terminar mañana
+def resolve_characteristical_polynomial_initial_value(equation : str, initial_values: int) -> sp.Function :
+    n = sp.symbols('n')
+    function_with_constants = resolve_characteristical_polynomial(equation)
+    for value in initial_values:
+        print(function_with_constants.subs(n,value))
+        
+"""
+
+"""
+def sub_fibonacci_sequence(sequence: list) -> list:
+    ans = list()
+    inc = 1
+    full_list = list()
+    for i in range(len(sequence)-1):
+        ans.extend([sequence[i],sequence[i+inc]])
+        next_number = ans[-1] + ans[-2]
+        while next_number in sequence:
+            ans.append(next_number)
+        if ans > 2:
+            full_list.append(ans)
+        ans.clear()
+    return full_list
+
+"""
+Comentar mañana / más tarde
+"""
+def checkOrder(sequence: list) -> bool:
+    for i in range(len(sequence)-1):
+        if sequence[i] > sequence[i+1] :
+            return False
+    return True
